f0c80a6afc5e738f8060c11a2d0109f751bd0010
Joe <V.josephraj26@gmail.com>
2024-10-30 22:53:16 -0400

    Made modals & authentication

--- authApp/urls.py
+++ authApp/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.landing_view, name='landingpage'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
+    path('home/', views.home_view, name='home'),
 ]
