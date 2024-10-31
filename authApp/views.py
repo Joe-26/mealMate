9ce6db6dd715c4bf73f9453630fc21741db5e0ef
Joe <V.josephraj26@gmail.com>
2024-10-31 16:25:43 -0400

    Added filter for logged in user for previous ingredients & recipes

--- authApp/views.py
+++ authApp/views.py
@@ -63,8 +63,8 @@ def logout_view(request):
 # Using the decorator
 @login_required
 def home_view(request):
-    ingredients = ingredientDb.objects.all()
-    recipes = recipeDb.objects.all()
+    ingredients = ingredientDb.objects.order_by('-ingredientId')[:8]
+    recipes = recipeDb.objects.order_by('-recipeId')[:8]
     return render(request, 'home.html', {'ingredients':ingredients, 'recipes':recipes})
 
 @login_required
