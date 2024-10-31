1036fa49468be57d4c8e09df5d2414af6eb1b2bd
Joe <V.josephraj26@gmail.com>
2024-10-31 14:16:09 -0400

    Added OpenAI Integration for generating 3 recipes

--- authApp/views.py
+++ authApp/views.py
@@ -8,6 +8,12 @@ from django.contrib.auth.models import User
 from .forms import RegisterForm, IngredientForm
 from .models import ingredientDb, recipeDb
 
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Landing Page View
 def landing_view(request):
     return render(request, 'landingpage.html')
@@ -61,6 +67,7 @@ def home_view(request):
 
 @login_required
 def newmeal_view(request):
+    context = None
     if request.method == "POST":
         ingredients_data = request.POST.get('ingredients', '')
         ingredients = ingredients_data.split(',')
@@ -70,5 +77,24 @@ def newmeal_view(request):
                     ingredientName=ingredient_name.strip(),
                     userName=request.user
                 )
+        calories = request.POST.get('calorieCount')
+        cuisine = request.POST.get('cuisine')
 
-    return render(request, 'newmeal.html')
+        print("Ingredients :", ingredients, " ;Calories :", calories, " ;Cuisine :", cuisine)
+        #context = "Ingredients :"+ ingredients_data+ " ;Calories :"+ calories+ " ;Cuisine :"+ cuisine
+        
+        client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+        completion = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant that should only answer questions related to recipes, ingredients, calories, and cuisines. Any other topics prompted, respond with 'Invalid question.'"},
+                {
+                    "role": "user",
+                    "content": f"Give me a 3 {cuisine} recipes that can be made with {ingredients} that are close to about {calories} calories. Give me a short description, ingredient list, calorie count, and instructions to cook the recipe in a pure json format without any introduction message starting and ending with curly brackets."
+                }
+            ]
+        )
+        context = completion.choices[0].message.content
+        
+
+    return render(request, 'newmeal.html', {'context':context})
