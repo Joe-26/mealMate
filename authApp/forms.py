f3b884022c4e193b80faf66b601f610e0de8f652
Joe <V.josephraj26@gmail.com>
2024-11-06 11:59:11 -0500

    Added style to Login & Register page

--- authApp/forms.py
+++ authApp/forms.py
@@ -4,13 +4,25 @@ from django.contrib.auth.models import User
 from .models import ingredientDb
 
 class RegisterForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput, label="Password")
-    password_confirm = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+            'class': 'rounded-lg w-full',
+            'style': 'color: black;',
+        }), label="Password")
+    password_confirm = forms.CharField(widget=forms.PasswordInput(attrs={
+            'class': 'rounded-lg w-full',
+            'style': 'color: black;',
+        }), label="Confirm Password")
 
     class Meta:
         model = User
         fields = ['username', 'password', 'password_confirm']
-    
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'rounded-lg w-full',
+                'style': 'color: black;'
+            }),
+        }
+
     def clean(self):
         cleaned_data = super().clean()
         password = cleaned_data.get('password')
