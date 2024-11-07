cddcdec871786c7ab1fc3529086a1d9fe0d5424c
Joe <V.josephraj26@gmail.com>
2024-11-06 22:04:58 -0500

    Ran collectstatic command to generate staticfiles_build folder

--- mealMate/settings.py
+++ mealMate/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'theme/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'theme/static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
